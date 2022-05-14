39758a0018c3bd429bb127f96d97fa9d08816970
chamix0 <charlie.chamizo@gmail.com>
2022-05-14 17:50:51 +0200

    buttons

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    #region Data
+
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private Image creditsImage;
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        creditsImage.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+
+    public void buttonShowCredits()
+    {
+        creditsImage.enabled = true;
+        print("aaaaaaaaaaaaaaaasd");
+    }
+
+    public void buttonQuitToMenu()
+    {
+    }
+
+    public void buttonQuit()
+    {
+        Application.Quit();
+    }
+
+    public void playButton()
+    {
+        creditsImage.enabled = false;
+        //screen shows loading
+        //buttons dissapear except for quit
+        //game starts
     }
 }
