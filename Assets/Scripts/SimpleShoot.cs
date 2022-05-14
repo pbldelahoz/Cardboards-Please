1eb0e5152c88ac43b4c43956ac968f87efbbde44
Juan José Pérez Abad <58952176+juanjomalouu@users.noreply.github.com>
2022-05-14 20:35:37 +0200

    scan done

--- Assets/Scripts/SimpleShoot.cs
+++ Assets/Scripts/SimpleShoot.cs
@@ -7,6 +7,7 @@ public class SimpleShoot : MonoBehaviour
     //public GameObject spotLight;
     private Light light;
     private BoxCollider box;
+    private bool pulsed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +23,9 @@ public class SimpleShoot : MonoBehaviour
 
     public void PullTheTriger()
     {
+        pulsed = true;
         light.enabled = !light.enabled;
+        box.enabled = !box.enabled;
         //Activamos la luz y el collider
         Debug.Log("ey");
     }
@@ -30,7 +33,18 @@ public class SimpleShoot : MonoBehaviour
     public void ReleaseTheTriger()
     {
         light.enabled = !light.enabled;
+        box.enabled = !box.enabled;
         //Desactivamos la luz y el collider
         Debug.Log("uyy");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Comprobar si es un objeto con código
+        if(other.gameObject.tag == "Barras" && pulsed)
+        {
+            Debug.Log("CORRECTO");
+            pulsed = false;
+        }
     }
 }
