0aa501f4b9909bbee1bcfde4045e19522be030db
Pablo (Darkops) <79284028+darkops96@users.noreply.github.com>
2022-05-14 15:03:04 +0200

    Caja casi hecha

--- Assets/Scripts/BoxScripts/FixSide.cs
+++ Assets/Scripts/BoxScripts/FixSide.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FixSide : MonoBehaviour
 {
     public GameObject boxSide;  
 
-    private void OnCollisionEnter(Collision other) {
+    private void OnTriggerEnter(Collider other)
+    {
         if(other.gameObject.Equals(boxSide) && boxSide.GetComponent<Grabbed>().IsGrabbed)
         {
-            boxSide.GetComponent<Rigidbody>().useGravity = false;
+            boxSide.GetComponent<Rigidbody>().isKinematic = true;
             HingeJoint joint = boxSide.GetComponent<HingeJoint>();
-            JointLimits jl = new JointLimits();
-            jl.min = -90;
-            jl.max = -90;
-            joint.limits = jl;            
+            joint.axis = Vector3.zero;
+            JointLimits limits = new JointLimits();
+            limits.max = -90;
+            limits.min = -90;
+            joint.limits = limits;            
+            boxSide.GetComponent<XRGrabInteractable>().throwOnDetach = true;
         }
     }
 }
