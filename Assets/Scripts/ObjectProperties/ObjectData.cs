f5befd024736c92c2ece218fb395118ba60a0b57
Carlos Chamizo Cano <49963607+chamix0@users.noreply.github.com>
2022-05-15 12:21:48 +0200

    Objects (#15)
    * some objects
    
    * objects

---
+++ Assets/Scripts/ObjectProperties/ObjectData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private string name;
+    [SerializeField] private string description;
+    [SerializeField] private float wheight;
+    [SerializeField] private string boxSize;
+    [SerializeField] private bool isEspecial;
+    
+    public
+        void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
