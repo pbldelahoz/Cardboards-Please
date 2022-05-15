679d9b88132a8b28f65366ee55e29c46b751edf2
Juan José Pérez Abad <58952176+juanjomalouu@users.noreply.github.com>
2022-05-15 03:18:47 +0200

    Done

---
+++ Assets/Destroyer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}
